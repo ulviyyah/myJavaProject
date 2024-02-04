e1444543c4caca9ea3f2aea3ccebec51a3a308a9
Ulviyya <cupcat2025@gmail.com>
2024-02-04 14:23:52 +0100

    Создала методы в задании 2

---
+++ javalessons/src/main/java/school/lesson2/NumberPrinter.java
@@ -0,0 +1,11 @@
+package school.lesson2;
+
+public class NumberPrinter {
+    public static void printNumberPrinter(int number) {
+        if (number >= 0) {
+            System.out.println("Положительное число");
+        } else {
+            System.out.println("Отрицательное число");
+        }
+    }
+}
---
+++ javalessons/src/main/java/school/lesson2/SumChecker.java
@@ -0,0 +1,8 @@
+package school.lesson2;
+
+public class SumChecker {
+    public static boolean isSumInRange(int num1, int num2) {
+        int sum = num1 + num2;
+        return sum >= 10 && sum <= 20;
+    }
+}
