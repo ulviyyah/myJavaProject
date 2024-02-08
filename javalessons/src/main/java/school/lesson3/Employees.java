40428e3032ba51ad3f25f8ac3d4074267f5c6256
Ulviyya <cupcat2025@gmail.com>
2024-02-08 15:40:43 +0100

    Вывела цикл информацию только о сотрудниках старше 40 лет

--- javalessons/src/main/java/school/lesson3/Employees.java
+++ javalessons/src/main/java/school/lesson3/Employees.java
@@ -1,6 +1,6 @@
 package school.lesson3;
 
-public class Employee {
+public class Employees {
     private String fullName;
     private String position;
     private String email;
@@ -8,7 +8,7 @@ public class Employee {
     private int salary;
     private int age;
 
-    public Employee(String fullName, String position, String email, String phone, int salary, int age) {
+    public Employees (String fullName, String position, String email, String phone, int salary, int age) {
         this.fullName = fullName;
         this.position = position;
         this.email = email;
@@ -28,15 +28,15 @@ public class Employee {
     }
 
     public static void main(String[] args) {
-        Employee[] employees = new Employee[5];
-        employees[0] = new Employee("Ivanov Ivan", "Engineer", "ivvanov@mailbox.com", "892312312", 30000, 30);
-        employees[1] = new Employee("Dyatlov Petr", "Manager", "dyatlov@mailbox.com", "892345678", 40000, 45);
-        employees[2] = new Employee("Sidorov Sidor", "Analyst", "sidorov@mailbox.com", "892356789", 35000, 35);
-        employees[3] = new Employee("Smirnov Sergey", "Developer", "smirnov@mailbox.com", "892367890", 45000, 50);
-        employees[4] = new Employee("Vasilyev Kirill", "Designer", "vasilyev@mailbox.com", "892378901", 32000, 25);
+        Employees[] employees = new Employees[5];
+        employees[0] = new Employees("Ivanov Ivan", "Engineer", "ivvanov@mailbox.com", "892312312", 30000, 30);
+        employees[1] = new Employees("Dyatlov Petr", "Manager", "dyatlov@mailbox.com", "892345678", 40000, 45);
+        employees[2] = new Employees("Sidorov Sidor", "Analyst", "sidorov@mailbox.com", "892356789", 35000, 35);
+        employees[3] = new Employees("Smirnov Sergey", "Developer", "smirnov@mailbox.com", "892367890", 45000, 50);
+        employees[4] = new Employees("Vasilyev Kirill", "Designer", "vasilyev@mailbox.com", "892378901", 32000, 25);
 
         System.out.println("Employees older than 40:");
-        for (Employee employee : employees) {
+        for (Employees employee : employees) {
             if (employee.age > 40) {
                 employee.printInfo();
             }
